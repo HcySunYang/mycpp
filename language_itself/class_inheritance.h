18661c9903b4d0fd329c01d21a8f305f38686b37
HcySunYang <HcySunYang@outlook.com>
2024-05-30 21:50:27 +0800

    update

--- language_itself/class_inheritance.h
+++ language_itself/class_inheritance.h
@@ -3,6 +3,7 @@
 
 /**
  * 1. Basic inheritance
+ * 2. Inheritance access specifiers
 */
 
 // Samples
@@ -15,6 +16,10 @@ class SuperClass {
     int getId() {
       return id_;
     }
+  protected:
+    int protectedId_;
+  private:
+    int privateId_;
 };
 
 // =================================================================
@@ -33,4 +38,47 @@ void test() {
   SubClass sub {1, "SubClass"};
   // access the member function that is inherited from the super class
   std::cout << sub.getId() << std::endl;
+}
+
+// =================================================================
+// 2. Inheritance access specifiers
+// =================================================================
+class A: public SuperClass {
+  public:
+    A(int id) : SuperClass(id) {
+      // access the public and protected member of the super class
+      id_ = 1;
+      protectedId_ = 1;
+    }
+};
+void test2() {
+  A a {1};
+  // access the public member of the super class
+  std::cout << a.id_ << std::endl;
+}
+
+class B: protected SuperClass {
+  public:
+    B(int id) : SuperClass(id) {
+      // access the public and protected member of the super class
+      id_ = 1;
+      protectedId_ = 1;
+    }
+};
+void test3() {
+  B b {1};
+  // b.id_; // error
+}
+
+class C: private SuperClass {
+  public:
+    C(int id): SuperClass(id) {
+      // access the public and protected member of the super class
+      id_ = 1;
+      protectedId_ = 1;
+    }
+};
+void test4() {
+  C c {1};
+  // c.id_; // error
 }
