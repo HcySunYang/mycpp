028914f64df4150b5c8fae0399b5577ffb41e891
HcySunYang <HcySunYang@outlook.com>
2024-10-31 12:49:32 +0800

    remove_spaces

--- bese_parctices/string_operations.cc
+++ bese_parctices/string_operations.cc
@@ -2,6 +2,7 @@
 
 /**
  * 1. Convert utf-16 to utf-8 or vice versa
+ * 2. Remove spaces from a string
  */
 
 // 1. Convert utf-16 to utf-8 or vice versa
@@ -10,4 +11,10 @@ void convert_strings() {
   std::wstring_convert<std::codecvt_utf8_utf16<char16_t>,char16_t> convert;
   std::string utf8 = convert.to_bytes(u"Hello, world");
   std::u16string utf16 = convert.from_bytes("Hello, world");
+}
+
+// 2. Remove spaces from a string
+void remove_spaces() {
+  std::string s = "Hello, world";
+  std::remove_if(s.begin(), s.end(), isspace);
 }
